aa9ba84d284d7d2bf8bf0207a07b1897a63c0bd5
Alex Martin <atonymartin20@yahoo.com>
2021-09-08 14:31:46 -0500

    Navbar now completed

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-import { NavbarOutsideContainer, NavbarInsideContainerLarge, NavbarLogo, NavbarLink, NavbarButton } from '../styledComponents/shared.js'
+import { NavbarOutsideContainer, NavbarInsideContainerLarge, NavbarInsideContainerSmall, NavbarLogo, NavbarLink, NavbarButton, NavbarIMG, 
+    NavbarInsideContainerSmallWithDrawer, NavbarCloseIcon, NavbarDividerLine, NavbarDrawerLink, NavbarDrawerButton } from '../styledComponents/shared.js'
 import Logo from '../../assets/shared/desktop/logo.svg';
+import Hamburger from '../../assets/shared/mobile/menu.svg';
+import CloseIcon from '../../assets/shared/mobile/close.svg';
+import { NavLink, Redirect } from 'react-router-dom';
 
 class Navbar extends React.Component {
     state = {
         drawerOpen: false,
+        redirectToContact: false,
     }
 
     openDrawer = (event) => {
@@ -22,11 +27,19 @@ class Navbar extends React.Component {
         })
     }
 
+    buttonClickToContact = (event) => {
+        event.preventDefault();
+        this.setState({
+            redirectToContact: true,
+        })
+    }
+
     render () {
         return (
             <NavbarOutsideContainer>
+                {this.state.redirectToContact === true ? <Redirect to='/contact' /> : null }
                 <NavbarInsideContainerLarge>
-                    <NavbarLogo to='/' src={Logo} alt='logo' />
+                    <NavLink to='/'><NavbarLogo src={Logo} alt='logo' /></NavLink>
 
                     <NavbarLink to='/pricing'>Pricing</NavbarLink>
 
@@ -34,11 +47,39 @@ class Navbar extends React.Component {
 
                     <NavbarLink to='/contact'>Contact</NavbarLink>
 
-                    <NavbarButton to='/contact'>
+                    <NavbarButton onClick={this.buttonClickToContact}>
                         Schedule a Demo
                     </NavbarButton>
                 </NavbarInsideContainerLarge>
 
+                { this.state.drawerOpen === false ? 
+                    <NavbarInsideContainerSmall>
+                        <NavLink to='/'><NavbarLogo src={Logo} alt='logo' /></NavLink>
+
+                        <NavbarIMG src={Hamburger} alt='hamburger' onClick={this.openDrawer} />
+
+                    </NavbarInsideContainerSmall>
+                : null
+                }
+
+                { this.state.drawerOpen === true ?
+                    <NavbarInsideContainerSmallWithDrawer>
+                        <NavbarCloseIcon  src={CloseIcon} alt='close icon' onClick={this.closeDrawer} />
+
+                        <NavbarDividerLine />
+
+                        <NavbarDrawerLink to='/pricing'>Pricing</NavbarDrawerLink>
+
+                        <NavbarDrawerLink to='/about'> About</NavbarDrawerLink>
+
+                        <NavbarDrawerLink to='/contact'>Contact</NavbarDrawerLink>
+
+                        <NavbarDrawerButton onClick={this.buttonClickToContact}>
+                            Schedule a Demo
+                        </NavbarDrawerButton>
+                    </NavbarInsideContainerSmallWithDrawer>
+                : null
+                }
 
             </NavbarOutsideContainer>
         )
