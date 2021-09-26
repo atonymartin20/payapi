ca4c26631261ee5ab3a18b7354d0115f87cf6459
Alex Martin <atonymartin20@yahoo.com>
2021-09-25 22:46:46 -0500

    Created Pricing Component

---
+++ src/components/pricing/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Navbar from '../navbar';
+
+import { SharedOutsideContainer } from '../styledComponents/shared.js'
+
+
+class Pricing extends React.Component {
+    render () {
+        return (
+            <SharedOutsideContainer>
+                <Navbar />
+
+
+            </SharedOutsideContainer>
+        )
+    }
+}
+
+export default Pricing;
