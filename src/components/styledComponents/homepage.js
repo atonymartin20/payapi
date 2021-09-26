8e8cdb9fd3ed6a658c3573a63c3e1c94cc26fec3
Alex Martin <atonymartin20@yahoo.com>
2021-09-25 22:46:31 -0500

    Added circle background to top of homepage

--- src/components/styledComponents/homepage.js
+++ src/components/styledComponents/homepage.js
@@ -1,1 +1,18 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import BGCircles from '../../assets/shared/desktop/bg-pattern-circle.svg';
+
+const HomepageTopPortion = styled.div`
+    width: 100%;
+    height: 737px;
+    margin-top: -88px;
+    display: flex;
+    background-image: url(${BGCircles});
+    background-position: 121% -172px; 
+    background-repeat: no-repeat;
+
+`;
+
+export {
+    HomepageTopPortion,
+}
