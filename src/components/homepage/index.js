cb66ffed3407204e5caeb4b27677359fff7c586a
Alex Martin <atonymartin20@yahoo.com>
2021-09-27 19:02:30 -0500

    Top Portion of homepage completed with hovers

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import PhoneIMG from '../../assets/home/desktop/illustration-phone-mockup.svg';
 import Navbar from '../navbar';
 
-import { SharedOutsideContainer } from '../styledComponents/shared.js'
+import { SharedOutsideContainer, SharedInsideContainer } from '../styledComponents/shared.js';
+import { HomepageTopPortion, HomepageTopPortionHeaderDiv, HomepageTopPortionHeaderH1, HomepageTopPortionHeaderSpan, HomepageTopPortionHeaderInput, HomepageTopPortionHeaderButton, HomepageTopPortionHeaderBottomSpan, HomepageTopPortionHeaderBottomDarkSpan, HomepageTopPortionPhoneIMG, HomepageSecondPortion, } from '../styledComponents/homepage.js';
 
 
 class Homepage extends React.Component {
@@ -12,7 +13,28 @@ class Homepage extends React.Component {
             <SharedOutsideContainer>
                 <Navbar />
 
+                <SharedInsideContainer>
+                    <HomepageTopPortion>
+                        <HomepageTopPortionHeaderDiv>
+                            <HomepageTopPortionHeaderH1>
+                                Start building with our APIs for absolutely free.
+                            </HomepageTopPortionHeaderH1>
+                            <HomepageTopPortionHeaderSpan>
+                                <HomepageTopPortionHeaderInput placeholder='Enter email address'/>
+                                <HomepageTopPortionHeaderButton>
+                                    Schedule a Demo
+                                </HomepageTopPortionHeaderButton>
+                            </HomepageTopPortionHeaderSpan>
+                            <HomepageTopPortionHeaderBottomSpan>
+                                Have any questions? <HomepageTopPortionHeaderBottomDarkSpan>Contact Us</HomepageTopPortionHeaderBottomDarkSpan>
+                            </HomepageTopPortionHeaderBottomSpan>
+                        </HomepageTopPortionHeaderDiv>
 
+                        <HomepageTopPortionPhoneIMG src={PhoneIMG} alt='phone' />
+                    </HomepageTopPortion>
+
+                    <HomepageSecondPortion></HomepageSecondPortion>
+                </SharedInsideContainer>
             </SharedOutsideContainer>
         )
     }
